269d53b67933e8edddd613d46fe8fbc3a1098985
In-S4n3 <tiagodcpp@gmail.com>
2023-12-18 20:55:19 +0000

    first commit

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,16 @@
+import { ActionIcon, Button } from '@mantine/core';
+
+export default function Home() {
+  return (
+    <section className="">
+      <ActionIcon
+        variant="gradient"
+        size="xl"
+        aria-label="Gradient action icon"
+        gradient={{ from: 'blue', to: 'violet', deg: 90 }}
+      >
+        love
+      </ActionIcon>
+    </section>
+  );
+}
---
+++ components/core/Header.tsx
@@ -0,0 +1,41 @@
+'use client';
+import { Burger, Button, Drawer, Group } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import Link from 'next/link';
+
+export default function Header() {
+  const [opened, { open, close }] = useDisclosure(false);
+  return (
+    <header className="py-10 bg-gray-900">
+      <nav className="max-w-8xl mx-auto flex items-center justify-between px-8">
+        <Link href="/" className="text-white">
+          <h1 className="text-base">BOILERPLATE</h1>
+        </Link>
+        <Group visibleFrom="xs">
+          <Button component="a" className="bg-purple-500" href="/login">
+            Login
+          </Button>
+        </Group>
+        <Burger
+          opened={opened}
+          onClick={open}
+          hiddenFrom="xs"
+          size="sm"
+          color="white"
+        />
+      </nav>
+      <Drawer
+        opened={opened}
+        onClose={close}
+        hiddenFrom="xs"
+        size="300px"
+        title="BOILERPLATE"
+        overlayProps={{ backgroundOpacity: 0.5, blur: 4 }}
+      >
+        <Button component="a" className="bg-purple-500" href="/login">
+          Login
+        </Button>
+      </Drawer>
+    </header>
+  );
+}
