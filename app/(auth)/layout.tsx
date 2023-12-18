6f2412aa1bc0c4b232bd7a1924cf7c136789bf6b
In-S4n3 <tiagodcpp@gmail.com>
2023-12-18 21:32:06 +0000

    added login ui

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,18 @@
+import Footer from '@/components/core/Footer';
+import Header from '@/components/core/Header';
+
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex min-h-screen flex-col bg-gradient-to-r from-indigo-500">
+      <Header />
+      <main className="flex justify-center flex-col items-center flex-1">
+        {children}
+      </main>
+      <Footer />
+    </div>
+  );
+}
